a05e8296c7fa0304ac1da68c561489d96e2fa7cb
mohsen mollayi <mmollayi@gmail.com>
2019-10-23 10:21:33 +0330

    added similarity_matrix data

--- template.R
+++ template.R
@@ -3,9 +3,5 @@ source("stsc_ulti.R")
 source("stsc.R")
 source("stsc_final.R")
 
-x <- scan("base.csv", sep = ",") %>% matrix(ncol = 20)
-distx <- as.matrix(dist(t(x)))
-
-#affinity_matrix
-W <- compute_affinity_mat(distx)
+W <- scan("similarity_matrix.csv", sep = ",") %>% matrix(ncol = 20)
 self_tuning_spectral_clustering(W)
